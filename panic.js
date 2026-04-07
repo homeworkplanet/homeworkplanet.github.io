bc867685d7c0ebc8c8664e5be3fc825bff8dfa09
homeworkplanet <elijahthebeast012@gmail.com>
2026-04-07 09:33:02 -0400

    Update panic.js

--- panic.js
+++ panic.js
@@ -1,5 +1,4 @@
-<script>
-function disguiseTab() {
+function applyDisguise() {
   document.title = "Google Docs";
 
   let favicon = document.querySelector("link[rel~='icon']");
@@ -10,15 +9,11 @@ function disguiseTab() {
   }
 
   favicon.href = "https://upload.wikimedia.org/wikipedia/commons/e/ec/GDocs_Favicon_Recreation.png?_=20220509151807";
-
-  // Save state so it persists across pages
-  localStorage.setItem("disguised", "true");
 }
 
-// Run automatically on every page load if previously enabled
+// Apply automatically on every page if activated
 window.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("disguised") === "true") {
-    disguiseTab();
+    applyDisguise();
   }
 });
-</script>
